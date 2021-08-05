60181db5c1097bd1db88fd4221dd9666aae2c918
d4nmc <danielmcgee.work@gmail.com>
2021-08-05 16:52:08 +0100

    MC: going to start a new hobby project revolving around testing and html / js later

---
+++ main/java/com/natwest/testingjava/TestingJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.testingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingJavaExampleApplication.class, args);
+	}
+
+}
---
+++ test/java/com/natwest/testingjava/TestingJavaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.testingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingJavaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
